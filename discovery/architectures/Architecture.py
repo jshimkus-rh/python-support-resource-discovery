1de068207763050e6c45780ac5234839d40451a7
Joe Shimkus <jshimkus@redhat.com>
2023-06-06 17:17:36 -0400

    Make default that of running machine.
    If not recognized the default from the defaults file will be used.
    
    Signed-off-by: Joe Shimkus <jshimkus@redhat.com>

--- discovery/architectures/Architecture.py
+++ discovery/architectures/Architecture.py
@@ -3,9 +3,13 @@
 #
 # Copyright Red Hat
 #
+import logging
 import os
+import platform
 
 from mill import defaults, factory
+
+log = logging.getLogger(__name__)
 
 ########################################################################
 class Architecture(factory.Factory, defaults.DefaultsFileInfo):
@@ -46,13 +50,26 @@ class Architecture(factory.Factory, defaults.DefaultsFileInfo):
   ####################################################################
   @classmethod
   def _defaultChoice(cls):
-    defaultArchitecture = cls.defaults(["architecture"]).lower()
+    default = platform.machine().lower()
+    # Differing systems may use different values for 'machine'.
+    # At present the only such known case is for ARM on Linux vs
+    # Darwin.
+    if default.startswith("arm"):
+      # Presumably Darwin; convert to the value used by Linux.
+      if default.endswith("64"):
+        default = "aarch64"
+      else:
+        default = "armhfp"
 
-    if not cls._isItemAvailable(defaultArchitecture):
-      raise ValueError("default architecture '{0}' not known"
-                        .format(defaultArchitecture))
+    if not cls._isItemAvailable(default):
+      log.info("your machine's architecture '{0}' is unknown".format(default))
+      default = cls.defaults(["architecture"]).lower()
 
-    return defaultArchitecture
+      if not cls._isItemAvailable(default):
+        raise ValueError("default architecture '{0}' not known"
+                          .format(default))
+
+    return default
 
   ####################################################################
   # Protected methods
